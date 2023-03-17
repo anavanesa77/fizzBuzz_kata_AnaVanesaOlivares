7a339f27bddd92e5cad7130fcabe40fcb0a49803
anavanesa77 <anav.olivares@gmail.com>
2023-03-17 11:26:19 +0000

    Initial commit

---
+++ src/main/java/com/example/fizzbuzz_kata_anavanesaolivares/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.example.fizzbuzz_kata_anavanesaolivares;
+
+
+import java.io.IOException;
+
+public class FizzBuzz {
+
+}
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+package com.example.fizzbuzz_kata_anavanesaolivares;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+}
