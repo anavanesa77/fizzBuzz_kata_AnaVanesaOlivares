fa637110c737c0cb2408da668285b8488fdc2906
anavanesa77 <anav.olivares@gmail.com>
2023-03-20 18:33:48 +0000

    Test in green

--- src/main/java/com/example/fizzbuzz_kata_anavanesaolivares/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz_kata_anavanesaolivares/FizzBuzz.java
@@ -1,8 +1,26 @@
 package com.example.fizzbuzz_kata_anavanesaolivares;
 
 
-import java.io.IOException;
-
 public class FizzBuzz {
 
-}
+    public static void main(String[] args) {
+
+    }
+
+    public String execute(int number) {
+        String fizz = "fizz";
+        String buzz = "buzz";
+        String fizzBuzz = "fizzBuzz";
+
+        if ((number % 3 == 0) & (number % 5 == 0 )) {
+                return fizzBuzz;
+        } else if (number % 3 == 0) {
+                return fizz;
+        } else if (number % 5 == 0) {
+            return buzz;
+        } else {
+            return String.valueOf(number);
+        }
+    }
+    }
+
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,7 +1,52 @@
-package com.example.fizzbuzz_kata_anavanesaolivares;
+import com.example.fizzbuzz_kata_anavanesaolivares.FizzBuzz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+
+    /* 1-1
+    *  3-fizz
+    *  5-buzz
+    *  15-fizzBuzz
+    */
+
+
+    @Test
+    public void testTeMuestreElMismoNumeroCuandoNoEsDivisibleEntreTresyCinco(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String number = fizzBuzz.execute(2);
+        Assertions.assertEquals("2",number);
+    }
+
+    @Test
+    public void testNumeroDivisibleEntreTresSeImprimeFizz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String number = fizzBuzz.execute(3);
+        Assertions.assertEquals("fizz",number);
+
+    }
+
+    @Test
+    public void testNumeroDivisibleEntreCincoSeImprimeBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String number = fizzBuzz.execute(5);
+        Assertions.assertEquals("buzz",number);
+
+    }
+
+    @Test
+    public void testNumeroDivisibleEntreCincoYTresSeImprimeFizzBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String number = fizzBuzz.execute(15);
+        Assertions.assertEquals("fizzBuzz",number);
+
+    }
+
+
 
 }
