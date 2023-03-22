ce65e2485026918b9661f18db3be26fbeab1f4f0
anavanesa77 <anav.olivares@gmail.com>
2023-03-22 19:31:58 +0000

    refactor code

--- src/main/java/com/example/fizzbuzz_kata_anavanesaolivares/FizzBuzz.java
+++ src/main/java/com/example/fizzbuzz_kata_anavanesaolivares/FizzBuzz.java
@@ -1,26 +1,26 @@
 package com.example.fizzbuzz_kata_anavanesaolivares;
 
-
 public class FizzBuzz {
 
     public static void main(String[] args) {
-
+        executable();
     }
 
-    public String execute(int number) {
-        String fizz = "fizz";
-        String buzz = "buzz";
-        String fizzBuzz = "fizzBuzz";
-
-        if ((number % 3 == 0) & (number % 5 == 0 )) {
-                return fizzBuzz;
-        } else if (number % 3 == 0) {
-                return fizz;
-        } else if (number % 5 == 0) {
-            return buzz;
-        } else {
-            return String.valueOf(number);
+    private static void executable(){
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(modifyValue(i));
         }
     }
+    public static String modifyValue (int number) {
+        if ((number % 3 == 0) & (number % 5 == 0)) {
+            return "fizzBuzz";
+        }
+        if (number % 3 == 0) {
+            return "fizz";
+        }
+        if (number % 5 == 0) {
+            return "buzz";
+        }
+        return String.valueOf(number);
     }
-
+}
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,52 +1,41 @@
 import com.example.fizzbuzz_kata_anavanesaolivares.FizzBuzz;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.Executor;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
     /* 1-1
-    *  3-fizz
-    *  5-buzz
-    *  15-fizzBuzz
-    */
-
+     *  3-fizz
+     *  5-buzz
+     *  15-fizzBuzz
+     */
 
     @Test
-    public void testTeMuestreElMismoNumeroCuandoNoEsDivisibleEntreTresyCinco(){
+    public void shows_same_number_when_it_is_not_divisible_by_three_and_five(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String number = fizzBuzz.execute(2);
+        String number = fizzBuzz.modifyValue(2);
         Assertions.assertEquals("2",number);
     }
 
     @Test
-    public void testNumeroDivisibleEntreTresSeImprimeFizz(){
+    public void number_divisible_by_three_is_printed_fizz(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String number = fizzBuzz.execute(3);
+        String number = fizzBuzz.modifyValue(3);
         Assertions.assertEquals("fizz",number);
-
     }
 
     @Test
-    public void testNumeroDivisibleEntreCincoSeImprimeBuzz(){
+    public void number_divisible_by_five_is_printed_buzz(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String number = fizzBuzz.execute(5);
+        String number = fizzBuzz.modifyValue(5);
         Assertions.assertEquals("buzz",number);
-
     }
 
     @Test
-    public void testNumeroDivisibleEntreCincoYTresSeImprimeFizzBuzz(){
+    public void number_divisible_by_five_and_three_is_printed_fizzBuzz(){
         FizzBuzz fizzBuzz = new FizzBuzz();
-        String number = fizzBuzz.execute(15);
+        String number = fizzBuzz.modifyValue(15);
         Assertions.assertEquals("fizzBuzz",number);
-
     }
-
-
-
 }
