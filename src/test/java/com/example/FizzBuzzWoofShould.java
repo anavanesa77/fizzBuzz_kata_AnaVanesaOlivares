02b805c26e5e5dd7157530f97ffbe80a6bb3b0a8
anavanesa77 <anav.olivares@gmail.com>
2023-03-28 19:59:31 +0100

    test red: print_fizz_if_contain_five

--- src/test/java/com/example/FizzBuzzWoofShould.java
+++ src/test/java/com/example/FizzBuzzWoofShould.java
@@ -16,6 +16,14 @@ class FizzBuzzWoofShould {
     public void print_fizz_when_the_number_is_same() {
         FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
         String number = fizzBuzzWoof.convertNumbersToWord(3);
-        Assertions.assertEquals("Fizz", number);
+        Assertions.assertEquals("Fizz(3)", number);
+    }
+
+    @Test
+    public void (){
+        FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
+        String number = fizzBuzzWoof.convertNumbersToWord(53);
+        Assertions.assertEquals("FizzFizz(53)", number);
+
     }
 }
