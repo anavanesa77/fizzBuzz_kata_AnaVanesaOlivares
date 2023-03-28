5d0d3c17736af9b331e32b2ae31d622e14ed98d8
anavanesa77 <anav.olivares@gmail.com>
2023-03-28 20:08:21 +0100

    test red: print_fizz_if_is_divisible_by_three

--- src/test/java/com/example/FizzBuzzWoofShould.java
+++ src/test/java/com/example/FizzBuzzWoofShould.java
@@ -13,17 +13,25 @@ class FizzBuzzWoofShould {
      */
 
     @Test
-    public void print_fizz_when_the_number_is_same() {
+    public void print_fizz_when_the_number_is_three() {
         FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
         String number = fizzBuzzWoof.convertNumbersToWord(3);
         Assertions.assertEquals("Fizz(3)", number);
     }
 
     @Test
-    public void print_fizz_if_contain_five (){
+    public void print_fizz_if_contain_three(){
         FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
         String number = fizzBuzzWoof.convertNumbersToWord(53);
         Assertions.assertEquals("Fizz(53)", number);
+
+    }
+
+    @Test
+    public void print_fizz_if_is_divisible_by_three(){
+        FizzBuzzWoof fizzBuzzWoof = new FizzBuzzWoof();
+        String number = fizzBuzzWoof.convertNumbersToWord(33);
+        Assertions.assertEquals("FizzFizz(33)", number);
 
     }
 }
